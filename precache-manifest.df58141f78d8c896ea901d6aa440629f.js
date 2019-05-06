06b5d2ed0f71e58e9582d42cf9b3590431c44a04
Sid Parmar <Sid@Sids-MacBook-Pro.local>
2019-05-06 16:48:00 -0700

    Updates

---
+++ precache-manifest.df58141f78d8c896ea901d6aa440629f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151a8abcccb46ba6a2bac9ac0dbb6719",
+    "url": "/LoLStats/index.html"
+  },
+  {
+    "revision": "af53acd92f24ae12ddaa",
+    "url": "/LoLStats/static/css/main.ab9654ab.chunk.css"
+  },
+  {
+    "revision": "575461626dd775a23a40",
+    "url": "/LoLStats/static/js/2.737cc4b1.chunk.js"
+  },
+  {
+    "revision": "af53acd92f24ae12ddaa",
+    "url": "/LoLStats/static/js/main.e150b06f.chunk.js"
+  },
+  {
+    "revision": "663a5d764ce0ff289ad0",
+    "url": "/LoLStats/static/js/runtime~main.eb826c9f.js"
+  },
+  {
+    "revision": "fcd8d05ca2a2981b7680f6b1b322c80e",
+    "url": "/LoLStats/static/media/loading.fcd8d05c.gif"
+  }
+]);
